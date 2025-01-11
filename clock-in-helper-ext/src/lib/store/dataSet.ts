386ce83ab178d8ff98ad57a9edb1ffed85e89c28
Ricardo Martines <rjstock@gmail.com>
2025-01-10 22:31:30 -0300

    chore: export dataSet types

--- clock-in-helper-ext/src/lib/store/dataSet.ts
+++ clock-in-helper-ext/src/lib/store/dataSet.ts
@@ -2,3 +2,5 @@ import { writable } from 'svelte/store';
 import { mockEntries } from '$lib/utils/mockEntries';
 
 export const dataSet = writable(mockEntries);
+export type DataSetType = typeof mockEntries;
+export type DataSetItemType = (typeof mockEntries)[0];
